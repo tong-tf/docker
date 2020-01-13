181f670201eca6829ccc773eb2f5b9a7d6fecead
tj <tj@bnd.cn>
2020-01-13 13:45:40 +0800

    add run.sh

---
+++ aosp/run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+usage()
+{
+	echo "run.sh 1404|1604"
+	echo "1404 to build ubuntu1404"
+	echo "1604 to build ubuntu1604"
+}
+
+case $1 in
+	1604)
+		docker build -t tong/aosp:1404 -f Dockerfile_aosp1404 .
+		;;
+	1404)
+		docker build -t tong/aosp:1604 -f Dockerfile_aosp1604 .
+		;;
+	*)
+		;;
+esac
+
+
+
+
+
